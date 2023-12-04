9f1f2db7ba8d9663cdc471afbe60dc1ba8a9dbb7
Jacob Gomez Carrillo <husky-coldjacob@hotmail.com>
2023-12-04 16:28:49 -0600

    Segundo Commit header modificado

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header(){
     return(
         
         <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto">
-            Seguimiento Pacientes {''} 
+            Seguimiento Pacientes {''} Con GitHub {''} 
             <span>Veterinaria</span>
         </h1>
         
